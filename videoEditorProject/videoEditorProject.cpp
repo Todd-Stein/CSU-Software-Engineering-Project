f83fa0be59691c0253614d45f99adc0fac02fca3
Todd-Stein <77040193+Todd-Stein@users.noreply.github.com>
2024-09-12 20:36:30 -0400

    Add project files.

---
+++ videoEditorProject/videoEditorProject.cpp
@@ -0,0 +1,12 @@
+﻿// videoEditorProject.cpp : Defines the entry point for the application.
+//
+
+#include "videoEditorProject.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
