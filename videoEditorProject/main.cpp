520d02bf0217a789d2619ecb87e705339f17b34e
Todd-Stein <77040193+Todd-Stein@users.noreply.github.com>
2024-09-17 11:47:16 -0400

    need to properly link static libraries

--- videoEditorProject/main.cpp
+++ videoEditorProject/main.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 
-#include <SDL.h>
+#include <SDL3/SDL.h>
 
-int main(int argc, char* [] argv) {
+int main(int argc, char** argv) {
 	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
 		std::cout << "Failed to initialize SDL" << std::endl;
 		return EXIT_FAILURE;
 	}
-	SDL_Window* window = SDL_CreateWindow("Main Window", 640, 480, SDL_WINDOW_MAXIMIZED);
+	//SDL_Window* window = SDL_CreateWindow("Main Window", 640, 480, SDL_WINDOW_MAXIMIZED);
 
-	SDL_ShowWindow(window);
+	//SDL_ShowWindow(window);
 
-	SDL_Event* evt;
-	while (SDL_PollEvent(evt)) {
+	//SDL_Event* evt;
+	//while (SDL_PollEvent(evt)) {
 		std::cout << "window test" << std::endl;
-	}
-	return EXIT_SUCCESS
+	//}
+		return EXIT_SUCCESS;
 }
